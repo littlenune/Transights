321020803f535b3fb6442eea231b08d00ab47cbb
Jirayut Leeupathumvong <jirayut_32547@hotmail.com>
2017-05-01 22:55:13 +0700

    add dropdown to edit or delete comment

--- src/components/ReviewItem.vue
+++ src/components/ReviewItem.vue
@@ -11,36 +11,66 @@
                     <strong>John Smith</strong> <small>@johnsmith</small>
                     <star-rating class="staritem":star-size="10" :read-only="true" :show-rating="false" :increment="1" :rating="5"></star-rating>
                 <br></p>
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+                <p v-if="!edit">
+                    {{ comment }} <small v-if="edited">- Edited</small>
                 </p>
+                <textarea v-else v-model="editComment"class="textarea"> {{ comment }}</textarea>
             </div>
-            <nav class="level is-mobile">
-                <div class="level-left">
-                    <a class="level-item">
-                        <span class="icon is-small"><i class="fa fa-reply"></i></span>
-                    </a>
-                    <a class="level-item">
-                        <span class="icon is-small"><i class="fa fa-retweet"></i></span>
-                    </a>
-                    <a class="level-item">
-                        <span class="icon is-small"><i class="fa fa-heart"></i></span>
-                    </a>
-                </div>
-            </nav>
+            <div v-if="edit">
+                <span class="button is-dark" @click="saveEdit()">Save</span>
+                <span class="button is-dark" @click="cancelEdit()">Cancel</span>
+            </div>
         </div>
         <div class="media-right">
-            <button class="delete"></button>
+            <dropdown :visible="visible" :position="position" @clickOut="visible = false">
+                <button class="delete link" @click="visiblePopup()"></button>
+                <div class="dialog" slot="dropdown">
+                    <aside class="menu"><ul class="menu-list">
+                        <li><a @click="editMsg">Edit</a></li>
+                        <li><a>Delete</a></li>
+                    </ul></aside>
+                </div>
+            </dropdown>
+            <!-- <button class="delete"></button> -->
         </div>
     </article>
 </template>
 
 <script>
 import StarRating from 'vue-star-rating'
+import dropdown from 'vue-my-dropdown';
 
 export default {
     components: {
-        StarRating
+        StarRating,
+        dropdown
+    },
+    data() {
+        return {
+            visible: false,
+            position: [ "center", "bottom", "right", "top" ],
+            comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.',
+            edit: false,
+            edited: false,
+            editComment: ''
+        }
+    },
+    methods: {
+        editMsg(){
+            this.edit = true
+            this.visiblePopup()
+        },
+        cancelEdit(){
+            this.edit = false
+        },
+        visiblePopup(){
+            this.visible = !this.visible
+        },
+        saveEdit(){
+            this.edited = true
+            this.edit = false
+            this.comment = this.editComment
+        }
     }
 }
 </script>
@@ -51,6 +81,29 @@ export default {
     .staritem {
         padding-left: 5px;
     }
+}
+
+.dialog{
+  background: #eee;
+  border: 1px solid #ccc;
+  padding: 0;
+  box-shadow: 1px 1px 6px 0 #999;
+}
+
+.menu-list {
+    line-height: 1.25;
+    li {
+        a {
+            padding: .5em .75em;
+            &:hover {
+                color: #000000;
+            }
+        }
+    }
+}
+
+small {
+    color: #999999;
 }
 
 </style>
