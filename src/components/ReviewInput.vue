676d16430cef1a00c7b170bd6262016259b874ac
Tunya <littlenune@gmail.com>
2017-05-12 16:29:52 +0700

    Add notification on review

--- src/components/ReviewInput.vue
+++ src/components/ReviewInput.vue
@@ -25,8 +25,26 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import Notification from 'vue-bulma-notification'
 import StarRating from 'vue-star-rating'
 import axios from 'axios'
+
+const NotificationComponent = Vue.extend(Notification)
+
+const openNotification = (propsData = {
+  title: '',
+  message: '',
+  type: '',
+  direction: '',
+  duration: 4500,
+  container: '.notifications'
+}) => {
+  return new NotificationComponent({
+    el: document.createElement('div'),
+    propsData
+  })
+}
 
 export default {
     props: ['dataPlace', 'dataUserLogin'],
@@ -55,11 +73,18 @@ export default {
                     })
                     
                 } else {
-                    console.log('please login first before review!')
+                    this.openNotificationWithType('Cannot review','Please login before review!','warning')
                 }
         },
         setRating(rating) {
           this.rating = rating;
+        },
+        openNotificationWithType (title,message,type) {
+            openNotification({
+                title: title,
+                message: message,
+                type: type
+            })
         }
     }
 }    
